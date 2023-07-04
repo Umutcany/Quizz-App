4ab5572ecae72fcb4941d4563530c88f5fee87f0
Umut Can Yilmaz <ylmazumutcan49@gmail.com>
2023-07-04 18:48:22 +0300

    highscores

--- end.js
+++ end.js
@@ -3,6 +3,13 @@ const saveScoreBtn = document.getElementById("saveScoreBtn")
 const finalScore = document.getElementById("finalScore")
 const mostRecentScore = localStorage.getItem('mostRecentScore')
 
+
+const highScores = JSON.parse(localStorage.getItem("highScores")) || []
+
+const MAX_HIGH_SCORES = 5
+
+
+
 finalScore.innerText=mostRecentScore
 
 
@@ -12,6 +19,19 @@ username.addEventListener("keyup", () => {
 })
 
 saveHighScore = (e) => {
-
+  
     e.preventDefault()
+
+
+    const score = {
+        score: Math.floor(Math.random()*100),
+        name: username.value
+    }
+
+    highScores.push(score)
+    highScores.sort((a, b) => b.score - a.score)   // eğer b a'dan büyükse b'yi a'nın önüne koy.
+    highScores.splice(5) // en yüksek 5 skoru al gerisine ihtiyacım yok.
+
+    localStorage.setItem('highScores', JSON.stringify(highScores))
+    window.location.assign('/')
 }
